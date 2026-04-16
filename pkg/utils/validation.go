d19073ed4a6e24939f0655e71f2d8a32e6af62aa
Samoilov Anton <samanton43@ya.ru>
2026-04-16 22:29:20 +0300

    deleted comments

--- pkg/utils/validation.go
+++ pkg/utils/validation.go
@@ -4,12 +4,10 @@ import "regexp"
 
 var urlRegex = regexp.MustCompile(`^(https?|ftp)://[^\s/$.?#].[^\s]*$`)
 
-// IsValidURL проверяет, является ли строка валидным URL.
 func IsValidURL(url string) bool {
 	return urlRegex.MatchString(url)
 }
 
-// IsValidShortID проверяет, что shortID состоит из 10 допустимых символов.
 func IsValidShortID(shortID string) bool {
 	if len(shortID) != 10 {
 		return false
